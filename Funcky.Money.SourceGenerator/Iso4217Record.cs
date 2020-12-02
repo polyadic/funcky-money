c096c01cf14b9fda563f80405f3425e247bba4dd
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-12-02 19:26:20 +0100

    Generate currency information using a source generator

---
+++ Funcky.Money.SourceGenerator/Iso4217Record.cs
@@ -0,0 +1,21 @@
+namespace Funcky.Money.SourceGenerator
+{
+    internal sealed record Iso4217Record
+    {
+        public Iso4217Record(string currencyName, string alphabeticCurrencyCode, int numericCurrencyCode, int? minorUnitDigits)
+        {
+            CurrencyName = currencyName;
+            AlphabeticCurrencyCode = alphabeticCurrencyCode;
+            NumericCurrencyCode = numericCurrencyCode;
+            MinorUnitDigits = minorUnitDigits;
+        }
+
+        public string CurrencyName { get; }
+
+        public string AlphabeticCurrencyCode { get; }
+
+        public int NumericCurrencyCode { get; }
+
+        public int? MinorUnitDigits { get; }
+    }
+}
---
+++ Funcky.Money.SourceGenerator/XmlNodeExtensions.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Xml;
+
+namespace Funcky.Money.SourceGenerator
+{
+    internal static class XmlNodeExtensions
+    {
+        public static IEnumerable<XmlNode> SelectNodesAsEnumerable(this XmlNode document, string xpath)
+        {
+            using var currencyNodes = document.SelectNodes(xpath);
+            return currencyNodes?.Cast<XmlNode>().ToImmutableArray() ?? Enumerable.Empty<XmlNode>();
+        }
+    }
+}
