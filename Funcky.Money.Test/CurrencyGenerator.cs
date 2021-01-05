c493e40024c9540c74c5e3b1c6b15a3d2905d5b4
Thomas Bruderer <thomas.bruderer@messerli.ch>
2021-01-05 14:49:47 +0100

    Currency generator now uses all possible currencies * AllCurrencies exposes the ImmutableArray type because we want to access by id and have it's length

--- Funcky.Money.Test/CurrencyGenerator.cs
+++ Funcky.Money.Test/CurrencyGenerator.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Linq;
 using FsCheck;
 
 namespace Funcky.Test
@@ -8,23 +8,8 @@ namespace Funcky.Test
         public static Arbitrary<Currency> MyCurrency()
         {
             return Arb.From(
-                from int id in Gen.Choose(0, 11)
-                select id switch
-                {
-                    0 => Currency.CHF,
-                    1 => Currency.USD,
-                    2 => Currency.EUR,
-                    3 => Currency.JPY,
-                    4 => Currency.GBP,
-                    5 => Currency.OMR,
-                    6 => Currency.VUV,
-                    7 => Currency.NOK,
-                    8 => Currency.CHE,
-                    9 => Currency.CHW,
-                    10 => Currency.XXX,
-                    11 => Currency.XAU,
-                    _ => throw new NotImplementedException("invalid id"),
-                });
+                from int id in Gen.Choose(0, Currency.AllCurrencies.Length - 1)
+                select Currency.AllCurrencies[id]);
         }
     }
 }
