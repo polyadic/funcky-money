c3ce1cdec34b9b7ef5f7c51d952c9d60876fe5f1
Thomas Bruderer <thomas.bruderer@messerli.ch>
2023-08-18 13:45:50 +0200

    Implement factor calculation. * Allow a context for Divide.

--- Funcky.Money/Extensions/MoneyDivisionExtension.cs
+++ Funcky.Money/Extensions/MoneyDivisionExtension.cs
@@ -1,3 +1,5 @@
+using Funcky.Monads;
+
 namespace Funcky;
 
 public static class MoneyDivisionExtension
@@ -5,8 +7,13 @@ public static class MoneyDivisionExtension
     public static IMoneyExpression Divide(this IMoneyExpression dividend, decimal divisor)
         => new MoneyProduct(dividend, 1.0m / divisor);
 
-    public static decimal Divide(this IMoneyExpression dividend, IMoneyExpression divisor)
-    {
-        return 0m;
-    }
+    public static decimal Divide(this IMoneyExpression dividend, IMoneyExpression divisor, Option<MoneyEvaluationContext> context = default)
+        => Divide(
+            dividend.Evaluate(context),
+            divisor.Evaluate(context));
+
+    private static decimal Divide(Money dividend, Money divisor)
+        => dividend.Currency == divisor.Currency
+            ? dividend.Amount / divisor.Amount
+            : throw new MissingExchangeRateException();
 }
