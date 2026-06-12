af9b75df02ca9f4e89610c31bcc6be35595ac505
Thomas Bruderer <thomas.bruderer@messerli.ch>
2026-06-12 20:08:14 +0200

    We can validate the division and immediatly throw, which was the behaviour before

--- Funcky.Money/Extensions/MoneyDivisionExtension.cs
+++ Funcky.Money/Extensions/MoneyDivisionExtension.cs
@@ -5,7 +5,9 @@ namespace Funcky;
 public static class MoneyDivisionExtension
 {
     public static IMoneyExpression Divide(this IMoneyExpression dividend, decimal divisor)
-        => new MoneyQuotient(dividend, divisor);
+        => divisor is 0.0m
+            ? throw new DivideByZeroException()
+            : new MoneyQuotient(dividend, divisor);
 
     public static decimal Divide(this IMoneyExpression dividend, IMoneyExpression divisor, Option<MoneyEvaluationContext> context = default)
         => Divide(
